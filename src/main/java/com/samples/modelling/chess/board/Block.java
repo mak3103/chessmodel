42bd17f359965d7984d9519495cc965760e578ae
Mayank <mak.mishra@gmail.com>
2016-06-07 14:27:17 +0530

    <Mayank>Added java comments for the Block class.

--- src/main/java/com/samples/modelling/chess/board/Block.java
+++ src/main/java/com/samples/modelling/chess/board/Block.java
@@ -3,10 +3,29 @@ package com.samples.modelling.chess.board;
 import com.samples.modelling.chess.Color;
 import com.samples.modelling.chess.chessmen.ChessMan;
 
+
+/**
+ * @author Mayank.
+ * This class represents one square block in the chess board.
+ *
+ */
 public class Block {
+	/**
+	 * 
+	 * The color of the square. 
+	 */
 	private final Color color;
+	
+	/**
+	 * 
+	 *The piece that is placed in the square. A null value means the square block is vacant. 
+	 */
 	private ChessMan chessman;
 
+	/**
+	 * 
+	 * Contructor to create the Block.
+	 */
 	public Block(Color aColor, ChessMan aPiece) {
 		color = aColor;
 		chessman = aPiece;
@@ -26,12 +45,21 @@ public class Block {
 		this.chessman = chessman;
 	}
 
+	/**
+	 * The method tells if the current block is occupied or not. 
+	 * @return
+	 */
 	public boolean isVacant()
 	{
 		if(chessman==null)
 			return true;
 		else return false;
 	}
+	
+	/**
+	 * The contructor for creating a vacant block. 
+	 * @param aColor
+	 */
 	public Block(Color aColor) {
 		color = aColor;
 	}

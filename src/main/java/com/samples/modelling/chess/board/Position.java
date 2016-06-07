0b97731e6747638e46158b20d994183f3d015913
Mayank <mak.mishra@gmail.com>
2016-06-07 15:04:09 +0530

    <Mayank>Added a new ExceptionType and added implementation of getPathTraversed() and getDisplacement() in a move(Positio.java).

--- src/main/java/com/samples/modelling/chess/board/Position.java
+++ src/main/java/com/samples/modelling/chess/board/Position.java
@@ -1,8 +1,10 @@
 package com.samples.modelling.chess.board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.samples.modelling.chess.PathStyle;
+import com.samples.modelling.chess.exceptions.InvalidPathStyleException;
 
 public class Position {
 
@@ -30,11 +32,17 @@ public class Position {
 		return yIndex;
 	}
 
+	/**
+	 * The method calculates the path style.
+	 * @param aStart - the start position.
+	 * @param aEnd - the end position.
+	 * @return - A path style that is applicable when a piece moves from aStart to aEnd.
+	 */
 	public static PathStyle getPathStyle(Position aStart, Position aEnd) {
 		if (isStraightPath(aStart, aEnd))
 			return PathStyle.STRAIGHT;
 	
-		/*Not yet implemented.
+		/*Not yet implemented. 
 		 * if (isSlantingPath(aStart, aEnd))
 			return PathStyle.SLANTING;
 		if (isHorseJump(aStart, aEnd))
@@ -44,13 +52,15 @@ public class Position {
 				"The path from source " + aStart + " to destination " + aEnd + " is not a valid path style.");
 	}
 
+	//Not needded for the first iteration.
+	// TODO - Implementation needs to be done.
 	private static boolean isHorseJump(Position aStart, Position aEnd) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	//Not needded for the first iteration.
+	// TODO - Implementation needs to be done.
 	private static boolean isSlantingPath(Position aStart, Position aEnd) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -61,12 +71,66 @@ public class Position {
 			return false;
 	}
 
+	
+	/**
+	 * Returns the no of blocks travelled by the piece if it goes from aStart to aEnd.
+	 * @param aStart - the starting position
+	 * @param aEnd - the final position of the piece.
+	 * @return - the no of blocks travelled by the piece in the move.
+	 */
 	public static int getDisplacement(Position aStart, Position aEnd) {
-		return 0;
+
+		PathStyle pathStyle = getPathStyle(aStart, aEnd);
+		int displacement = 0;
+		
+		switch(pathStyle)
+		{
+			case STRAIGHT : if(aStart.getxIndex()==aEnd.getxIndex())
+			{
+				displacement = Math.abs(aStart.getyIndex()-aEnd.getyIndex());
+			}
+			else
+			{
+				displacement = Math.abs(aStart.getxIndex()-aEnd.getxIndex());
+			}
+			case SLANTING : throw new UnsupportedOperationException("The slanting path style is not yet supported."); 
+				
+			case HORSEJUMP : throw new UnsupportedOperationException("The horsejump path style is not yet supported."); 
+		
+		}
+		return displacement;
 	}
 
+	/**
+	 * The method returns the list of positions covered by the piece in a move.
+	 * @param aStart - the start position.
+	 * @param aEnd - the end position.
+	 * @return - the list of positions travelled by the piece.
+	 */
 	public static List<Position> getPathTraversed(Position aStart, Position aEnd) {
-		// TODO Auto-generated method stub
-		return null;
+		PathStyle pathStyle = getPathStyle(aStart, aEnd);
+		List<Position> path = new ArrayList<Position>();
+		switch(pathStyle)
+		{
+			case STRAIGHT : if(aStart.getxIndex()==aEnd.getxIndex())
+			{
+				for(int i=aStart.getyIndex();i<aEnd.getyIndex();i++)
+				{
+					path.add(new Position(aStart.getxIndex()+"-"+i));
+				}
+			}
+			else
+			{
+				for(int i=aStart.getxIndex();i<aEnd.getxIndex();i++)
+				{
+					path.add(new Position(i+"-"+aStart.getyIndex()));
+				}
+			}
+			case SLANTING : throw new UnsupportedOperationException("The slanting path style is not yet supported."); 
+				
+			case HORSEJUMP : throw new UnsupportedOperationException("The horsejump path style is not yet supported."); 
+		
+		}
+		return path;
 	}
 }
